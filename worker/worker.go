7782f7c03a342bef592f840c46343cd366a8d57b
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-04-26 10:45:33 +0200

    Add worker functions

--- worker/worker.go
+++ worker/worker.go
@@ -1,4 +1,46 @@
 package worker
 
-func worker() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Result struct {
+	Line    string
+	Path    string
+	LineNum int
+}
+
+type Results struct {
+	Inner []Result
+}
+
+func NewResult(line string, lineNum int, path string) Result {
+	return Result{line, path, lineNum}
+}
+
+func FindInFile(path string, find string) *Results {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	results := Results{make([]Result, 0)}
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), find) {
+			r := NewResult(scanner.Text(), lineNum, path)
+			results.Inner = append(results.Inner, r)
+		}
+		lineNum += 1
+	}
+	if len(results.Inner) == 0 {
+		return nil
+	} else {
+		return &results
+	}
 }
